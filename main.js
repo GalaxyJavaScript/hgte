b4a7a4a255065e360a20c278d09dd45bec002e45
GalaxyJavaScript <105827309+GalaxyJavaScript@users.noreply.github.com>
2023-02-22 18:31:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,50 @@ function modelLoaded(){
     console.log('Model loaded!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
 }
 
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error){
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        predection_1 = results[0].label;
+        predection_2 = results[1].label;
+        speak();
+        if(results[0].label == "Fist")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9994;";
+        } 
+        if(results[0].label == "Hand")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9995;";
+        } 
+        if(results[0].label == "Peace")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        } 
+
+        if(results[1].label == "Fist")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#9994;";
+        } 
+        if(results[1].label == "Hand")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#9995;";
+        } 
+        if(results[1].label == "Peace")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#9996;";
+        } 
+    }
+    }
+
 function speak(){
     var synth = window.speakSynthesis;
     speak_data_1 = "The First  predection is" + predection_1;
